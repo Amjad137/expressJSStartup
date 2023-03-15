115e32d51a3a74c20673117a6bfb1db9f0c241e5
Amjad137 <mohammedamjath137@gmail.com>
2023-03-15 16:23:50 +0530

    Initiated

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+
+const app=express();
+app.use(express.json());
+
+const port=1836;
+
+const myArray=["one", "two", "three"];
+
+app.get("/methds",(req,res) => {
+    res.status(200).send(myArray);
+});
+
+app.post("/methds",(req,res) => {
+    let mynewArray = req.body.Amjad;
+    myArray.push(mynewArray);
+    res.status(200).send({ message : "Task Added Successfully" });
+});
+
+app.delete("/methds",(req,res) => {
+    res.status(200).send(myArray);
+});
+
+app.listen(port, () => {
+    console.log(`The expressjs server started on Port: ${port}`);
+});
+
+// http://localhost:2220
